09844adf97fdd103145963910d3fe332453c0c32
James Brooks <james@gooddogdesign.com>
2012-10-25 10:45:54 -0700

    Hook support now accepts any callable code (lambda/proc/method). Added checking for callable code (#call and #arity must be present). Hooks#fire now accepts a data argument which is sent to all registrations

--- lib/git-runner/hooks.rb
+++ lib/git-runner/hooks.rb
@@ -7,13 +7,29 @@ module GitRunner
       @registrations ||= Hash.new { |hash, key| hash[key] = [] }
     end
 
-    def register(name, object, method)
-      registrations[name] << [ object, method ]
+    def register(name, callable)
+      unless callable.respond_to?(:call) && callable.respond_to?(:arity)
+        raise InvalidCallable.new("Supplied callable is not callable, should respond to #call and #arity")
+      end
+
+      if callable.arity != 1
+        raise InvalidCallable.new("Supplied callable takes #{callable.arity} argument(s), should accept only 1.")
+      end
+
+      registrations[name] << callable
     end
 
-    def fire(name)
+    def fire(name, data=nil)
       return unless registrations.keys.include?(name)
-      registrations[name].each { |object, method| object.send(method) }
+      registrations[name].each { |callable| callable.call(data) }
+    end
+  end
+end
+
+
+module GitRunner
+  module Hooks
+    class InvalidCallable < StandardError
     end
   end
 end
